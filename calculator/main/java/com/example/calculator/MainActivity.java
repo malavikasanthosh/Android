c552fa11d68fe289fb3faa41aaf6fa25108fbe4b
malavikasanthosh <77485867+malavikasanthosh@users.noreply.github.com>
2022-02-01 15:27:16 +0530

    Add files via upload

--- calculator/main/java/com/example/calculator/MainActivity.java
+++ calculator/main/java/com/example/calculator/MainActivity.java
@@ -6,217 +6,133 @@ import android.hardware.SensorAdditionalInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    double input1=0,input2=0;
-TextView txt;
+    double sum=0.0;
+    String op,text;
+    EditText res;
 Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,add,sub,mul,div,dot,clr,equ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b0=(Button) findViewById(R.id.b0);
-        b1=(Button) findViewById(R.id.b1);
-        b2=(Button) findViewById(R.id.b2);
-        b3=(Button) findViewById(R.id.b3);
-        b4=(Button) findViewById(R.id.b4);
-        b5=(Button) findViewById(R.id.b5);
-        b6=(Button) findViewById(R.id.b6);
-        b7=(Button) findViewById(R.id.b7);
-        b8=(Button) findViewById(R.id.b8);
-        b9=(Button) findViewById(R.id.b9);
-        add=(Button) findViewById(R.id.badd);
-        sub=(Button) findViewById(R.id.bsub);
-        div=(Button) findViewById(R.id.bdiv);
-        mul=(Button) findViewById(R.id.bmul);
-        dot=(Button) findViewById(R.id.bdot);
-        clr=(Button) findViewById(R.id.bc);
-        equ=(Button) findViewById(R.id.bequ);
-        txt=(TextView) findViewById(R.id.edt1);
+        b0 = (Button) findViewById(R.id.b0);
+        b1 = (Button) findViewById(R.id.b1);
+        b2 = (Button) findViewById(R.id.b2);
+        b3 = (Button) findViewById(R.id.b3);
+        b4 = (Button) findViewById(R.id.b4);
+        b5 = (Button) findViewById(R.id.b5);
+        b6 = (Button) findViewById(R.id.b6);
+        b7 = (Button) findViewById(R.id.b7);
+        b8 = (Button) findViewById(R.id.b8);
+        b9 = (Button) findViewById(R.id.b9);
+        add = (Button) findViewById(R.id.badd);
+        sub = (Button) findViewById(R.id.bsub);
+        div = (Button) findViewById(R.id.bdiv);
+        mul = (Button) findViewById(R.id.bmul);
+        dot = (Button) findViewById(R.id.bdot);
+        clr = (Button) findViewById(R.id.bc);
+        equ = (Button) findViewById(R.id.bequ);
+        res = (EditText) findViewById(R.id.edt1);
 
-        b0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                txt.setText(txt.getText()+"0");
-            }
-        });
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                txt.setText(txt.getText()+"1");
-            }
-        });
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                txt.setText(txt.getText()+"2");
-            }
-        });
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                txt.setText(txt.getText()+"3");
-            }
-        });
-        b4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                txt.setText(txt.getText()+"4");
-            }
-        });
-        b5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                txt.setText(txt.getText()+"5");
-            }
-        });
-        b6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                txt.setText(txt.getText()+"6");
-            }
-        });
-        b7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                txt.setText(txt.getText()+"7");
-            }
-        });
-        b8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                txt.setText(txt.getText()+"8");
-            }
-        });
-        b9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                txt.setText(txt.getText()+"9");
-            }
-        });
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(txt.getText().length()!=0)
-                {
-                    input1=Float.parseFloat(txt.getText()+"");
-                    boolean Addition = true;
-                    boolean decimal=false;
-                    txt.setText(null);
-                }
-            }
-        });
-
-
-        sub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(txt.getText().length()!=0)
-                {
-                    input1=Float.parseFloat(txt.getText()+"");
-                    boolean Subtract = true;
-                    boolean decimal=false;
-                    txt.setText(null);
-                }
-            }
-        });
-        mul.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(txt.getText().length()!=0)
-                {
-                    input1=Float.parseFloat(txt.getText()+"");
-                    boolean Multiplication = true;
-                    boolean decimal=false;
-                    txt.setText(null);
-                }
-            }
-        });
-        div.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(txt.getText().length()!=0)
-                {
-                    input1=Float.parseFloat(txt.getText()+"");
-                    boolean Division= true;
-                    boolean decimal=false;
-                    txt.setText(null);
-                }
-            }
-        });
-
-        equ.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                boolean Addition = true;
-                boolean Substract=true;
-                boolean Multiplication=true;
-                boolean Division=true;
-
-                if(Addition || Substract || Multiplication || Division )
-                {
-                    input2=Float.parseFloat(txt.getText()+"")  ;
-
-                }
-                if(Addition)
-                {
-                    txt.setText(input1 + input2+"");
-                    Addition=false;
-
-                }
-                if(Substract)
-                {
-                    txt.setText(input1 - input2+"");
-                    Substract=false;
-
-                }
-                if(Multiplication)
-                {
-                    txt.setText(input1 * input2+"");
-                    Multiplication=false;
-
-                }
-                if(Division)
-                {
-                    txt.setText(input1 / input2+"");
-                    Division=false;
-
-                }
-            }
-        });
-        clr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                txt.setText("");
-                input1=0.0;
-                input2=0.0;
-            }
-        });
-        dot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                boolean decimal = false;
-                if (decimal) {
-                } else {
-                    txt.setText(txt.getText() + ".");
-
-                    decimal = true;
-                }
-            }
-
-        });
-
-
-
-
-
-
-
-
-
-
-
+        b0.setOnClickListener(this);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        b6.setOnClickListener(this);
+        b7.setOnClickListener(this);
+        b8.setOnClickListener(this);
+        b9.setOnClickListener(this);
+        dot.setOnClickListener(this);
+        clr.setOnClickListener(this);
+        equ.setOnClickListener(this);
+        add.setOnClickListener(this);
+        sub.setOnClickListener(this);
+        div.setOnClickListener(this);
+        mul.setOnClickListener(this);
     }
-}
+
+        public void onClick (View v)
+        {
+            text = res.getText().toString();
+            if (v.getId() == R.id.b0)
+                res.setText(text + "0");
+
+            if (v.getId() == R.id.b1)
+                res.setText(text + "1");
+
+            if (v.getId() == R.id.b2)
+                res.setText(text + "2");
+
+            if (v.getId() == R.id.b3)
+                res.setText(text + "3");
+            if (v.getId() == R.id.b4)
+                res.setText(text + "4");
+            if (v.getId() == R.id.b5)
+                res.setText(text + "5");
+            if (v.getId() == R.id.b6)
+                res.setText(text + "6");
+
+            if (v.getId() == R.id.b7)
+                res.setText(text + "7");
+            if (v.getId() == R.id.b8)
+                res.setText(text + "8");
+
+            if (v.getId() == R.id.b9)
+                res.setText(text + "9");
+            if (v.getId() == R.id.bdot)
+                res.setText(text + ".");
+
+
+            if (v.getId() == R.id.badd) {
+                res.setText(text + "+");
+                sum = Double.valueOf(res.getText().toString());
+                res.setText("");
+
+            }
+            if (v.getId() == R.id.bsub) {
+                res.setText(text + "-");
+                sum = Double.valueOf(res.getText().toString());
+                res.setText("");
+
+            }
+            if (v.getId() == R.id.bmul) {
+                res.setText(text + "*");
+                sum = Double.valueOf(res.getText().toString());
+                res.setText("");
+
+            }
+            if (v.getId() == R.id.bdiv) {
+                res.setText(text + "/");
+                sum = Double.valueOf(res.getText().toString());
+                res.setText("");
+
+            }
+            if (v.getId() == R.id.bequ) {
+                if (op.equals("-")) {
+                    sum = sum - Float.valueOf(res.getText().toString());
+                    res.setText(String.valueOf(sum));
+                    sum = 0.0;
+                } else if (op.equals("+")) {
+                    sum = sum + Float.valueOf(res.getText().toString());
+                    res.setText(String.valueOf(sum));
+                    sum = 0.0;
+                } else if (op.equals("*")) {
+                    sum = sum * Float.valueOf(res.getText().toString());
+                    res.setText(String.valueOf(sum));
+                    sum = 0.0;
+                } else if (op.equals("/")) {
+                    sum = sum / Float.valueOf(res.getText().toString());
+                    res.setText(String.valueOf(sum));
+                    sum = 0.0;
+                }
+            }
+            if (v.getId() == R.id.bc)
+                res.setText("");
+        }
+    }
